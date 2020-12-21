ddf88fe40d29fc61ec819bc71aff4d2fa3eaa775
Javier Reyes <jhreyes483@misena.edu.co>
2020-12-20 20:23:25 -0500

    Shore retas icons

--- vista/plantillas/cuerpo/finhtml.php
+++ vista/plantillas/cuerpo/finhtml.php
@@ -3,6 +3,6 @@
    <script src="../public/js/jquery-3.5.1.min.js"></script>
    <script src="../public/js/popper.min.js"></script>
    <script src="../public/js/bootstrap.min.js"></script>
-   <script src="../public/js/kit-fontawesome.js"></script>
+   <script src="../public/js/fontawasome-ico.js"></script>
    </body>
    </html>
